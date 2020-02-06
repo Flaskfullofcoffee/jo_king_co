95dd496f15e7a5a2d9bee196057e00c08b074aea
Flaskfullofcoffee <zdobbs3@gmail.com>
2020-02-06 16:21:03 -0700

    fixed svg width issue

--- src/pages/services.js
+++ src/pages/services.js
@@ -6,8 +6,9 @@ const Services = () => {
   return (
     <div className={servicesStyles.container}>
       <div id='services' className={servicesStyles.header}>
-        <h2 className={servicesStyles.title}>– Monthly services –</h2>
+        <h2 className={servicesStyles.title}>Monthly services</h2>
         <p className={servicesStyles.subtext}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <hr className={servicesStyles.detail}/>
       </div>
       <div className={servicesStyles.illustration}>
         <div className={servicesStyles.object}></div>
