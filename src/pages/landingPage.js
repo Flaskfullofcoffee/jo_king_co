e4c0528945cc72a33daf8c73d501242ac1d5f18b
Flaskfullofcoffee <zdobbs3@gmail.com>
2020-02-25 18:40:27 -0700

    setting styles for all sections on mobile devices

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -5,6 +5,7 @@ const LandingPage = () => {
 
   return (
     <div className={landingPage.container}>
+    <div className={landingPage.logo}></div>
       <div className={landingPage.grabber}>
         <h2 className={landingPage.title}>Social Media <br />Managment</h2>
         <p className={landingPage.subtext}>Simple solutions for complex problems.</p>
--- src/pages/services.js
+++ src/pages/services.js
@@ -13,6 +13,23 @@ const Services = () => {
       <div className={servicesStyles.illustration}>
         <div className={servicesStyles.object}></div>
       </div>
+      <div className={servicesStyles.listWrapper}>
+        <ul className={servicesStyles.list_1}>
+          <li className={servicesStyles.listItem}>Insta stories</li>
+          <li className={servicesStyles.listItem}>Site loads</li>
+          <li className={servicesStyles.listItem}>Site management</li>
+          <li className={servicesStyles.listItem}>Graphics for sales</li>
+          <li className={servicesStyles.listItem}>Wholesale management</li>
+          <li className={servicesStyles.listItem}>Rep. management</li>
+        </ul>
+        <ul className={servicesStyles.list_2}>
+          <li className={servicesStyles.listItem}>Instagram & Facebook<br/>posts w/ engagement</li>
+          <li className={servicesStyles.listItem}>Email marketing</li>
+          <li className={servicesStyles.listItem}>Customer service</li>
+          <li className={servicesStyles.listItem}>Collab management</li>
+          <li className={servicesStyles.listItem}>Text marketing</li>
+        </ul>
+      </div>
     </div>
   )
 }
