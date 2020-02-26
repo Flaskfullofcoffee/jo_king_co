3b8bbd2dce72e83d1171fbc05aba038c68d1f1fa
Flaskfullofcoffee <zdobbs3@gmail.com>
2020-02-25 20:31:29 -0700

    finished most styling for mobil, need to fix z-index on certain elements

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -1,11 +1,18 @@
 import React from "react"
+import { Link } from 'gatsby'
 import landingPage from './landingPage.module.scss'
 
 const LandingPage = () => {
 
   return (
     <div className={landingPage.container}>
-    <div className={landingPage.logo}></div>
+      <div className={landingPage.logo}></div>
+      <div className={landingPage.navigation}>
+        <Link className={landingPage.link} activeClassName={landingPage.active} to='/'>Home</Link>
+        <Link className={landingPage.link} activeClassName={landingPage.active} to='#about'>About</Link>
+        <Link className={landingPage.link} activeClassName={landingPage.active} to='#services'>Services</Link>
+        <Link className={landingPage.link} activeClassName={landingPage.active} to='#contact'>Contact</Link>
+      </div>
       <div className={landingPage.grabber}>
         <h2 className={landingPage.title}>Social Media <br />Managment</h2>
         <p className={landingPage.subtext}>Simple solutions for complex problems.</p>
